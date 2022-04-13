39f084e55268d55e0f12a34834f2724a344304a6
aditya kalbhor <adikalbhor1010@gmail.com>
2022-04-12 23:41:06 -0700

    Committing aditya kalbhor to adikalbhor1010@gmail.com

--- Godavari/02_bitwise/main.c
+++ Godavari/02_bitwise/main.c
@@ -5,12 +5,13 @@
  int main()
 {
 
- int a,b,c,d,e,f;
+ int a,b,c,d,e,f,g,h,i;
+  printf("Enter you answer in yes:-0 and no:-1\n");
   printf("DO you want admission in next grade?\n");
   scanf("%d",&a);
-   printf("You passed in math? \n");
+  printf("You passed in math? \n");
   scanf("%d",&b);
-    printf("You passed in Physics?\n");
+  printf("You passed in Physics?\n");
   scanf("%d",&d);
     printf("You passed in Chemistry?\n");
   scanf("%d",&e);
@@ -23,6 +24,7 @@
   printf("You passed in Arts?\n");
   scanf("%d",&i);
 
+    if()
  return 0;
 }
 
