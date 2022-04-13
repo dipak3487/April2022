270293e77ac860cf39e0af89a1d6278385d262a1
aditya kalbhor <adikalbhor1010@gmail.com>
2022-04-12 23:23:18 -0700

    Committing aditya kalbhor to adikalbhor1010@gmail.com

---
+++ Godavari/02_bitwise/main.c
@@ -0,0 +1,23 @@
+/*Aditya Kalbhor
+
+*/
+
+ int main()
+{
+
+ int a,b,c,d,e,f;
+  printf("DO you want admission in next grade?\n");
+  scanf("%d",&a);
+   printf("DO you want admission in next grade?\n");
+  scanf("%d",&b);
+    printf("DO you want admission in next grade?\n");
+  scanf("%d",&d);
+    printf("DO you want admission in next grade?\n");
+  scanf("%d",&e);
+    printf("DO you want admission in next grade?\n");
+  scanf("%d",&f); 
+     printf("DO you want admission in next grade?\n");
+  scanf("%d",&a);
+ return 0;
+}
+
