12998a93e27ad24262438ab67861820f0ce91913
Dipak Zope <dipak.springpeople@gmail.com>
2022-04-13 15:41:09 +0530

    bitwise v03

--- Asignments/_ref/02_bitwise/main.c
+++ Asignments/_ref/02_bitwise/main.c
@@ -13,6 +13,8 @@ int main()
 {
 	//int a, b, c, d, e, f, g, h;
 	unsigned char ans = 0;
+	unsigned char mask = 1;
+	unsigned char passCount = 0;
 
 	ans = takeUserInput("a) Want admission in next grade? : "); 
 
@@ -49,7 +51,23 @@ int main()
 	// 1110 0000 
 	// e0
 
-	if( (ans & 0xe0) == 0xe0 )
+	//if( (ans & 0xe0) == 0xe0 )	// maths and pysics must pass.
+
+					  // a==1 && (b+c+d+e+f+g+h) >= 6)
+	
+	//mask = 1;
+	// xxxx xxxx
+	// 0000 0001
+	// 0000 0010
+
+	for(int i=0; i<7; i++)
+	{
+		if(ans & mask) passCount++;
+		mask <<= 1;
+	}
+	printf("passCount=%d\n",passCount);
+
+	if(0xff == ans || ((0x80&ans) && (6 >= passCount) ))
 	{
         printf("You got the admission!\n");
     }
