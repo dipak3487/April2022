d7b2b56c41dbd945ca1f6c645f43a15469cf70ea
praveen7762 <pks19023@gmail.com>
2022-04-13 12:54:48 +0530

    Committing to team Narmada

---
+++ Narmada/02_bitwise/main.c
@@ -0,0 +1,55 @@
+/*Praveen
+Nishant
+Gopal
+Vinit
+Rohit
+Aishwarya
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\n\n\n\t\t This program is from team Narmada\n\n\n\n");
+	int a,b,c,d,e,f,g,h;
+	
+	printf("a) Want to take admission in next grade\n\n");
+	scanf("%d", &a);
+	printf("b) You passed in math?\n\n");
+	scanf("%d", &b);
+	printf("b) You passed in physics?\n\n");
+        scanf("%d", &c);
+	printf("b) You passed in chemistry?\n\n");
+        scanf("%d", &d);
+	printf("b) You passed in biology?\n\n");
+        scanf("%d", &e);
+	printf("b) You passed in english?\n\n");
+        scanf("%d", &f);
+	printf("b) You passed in history?\n\n");
+        scanf("%d", &g);
+	printf("b) You passed in arts?\n\n");
+        scanf("%d", &h);
+
+
+	printf("Your answers are :\n\");
+	printf("Ans (a) =%d\n ", a);
+        printf("Ans (b) =%d\n ", b);
+        printf("Ans (c) =%d\n ", c);
+        printf("Ans (d) =%d\n ", d);
+        printf("Ans (e) =%d\n ", e);
+        printf("Ans (f) =%d\n ", f);
+	printf("Ans (g) =%d\n ", g);
+	printf("Ans (h) =%d\n ", h);
+
+	if(a && b && c && d && e && f && g && h == 1)
+	{
+		printf("You got the admission\n");
+	}
+	else
+	{
+		printf("You did not get the admission\n");
+	}
+	return 0;
+}
+
