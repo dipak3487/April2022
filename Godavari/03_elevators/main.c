2310edbffb0ccd53f210e3b60596a3d04345ef79
aditya kalbhor <adikalbhor1010@gmail.com>
2022-04-18 06:14:51 -0700

    Committing aditya kalbhor to adikalbhor1010@gmail.com

--- Godavari/03_elevators/main.c
+++ Godavari/03_elevators/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 typedef enum State { Idle=0, UP, DOWN} State;
 typedef struct Command {
       int data;
@@ -8,8 +9,10 @@ typedef struct Command {
 int currentFloor;
 int totalFloors;
 State currentState;
+Command *head = NULL;
 
-int takeFloors(int m)
+
+int takeFloors()
 {
         //take number of floors available (1..99) in totalFloors
 
@@ -19,14 +22,14 @@ int takeFloors(int m)
             //take input
             //validate the input (1..99)
             int F;
-	printf("enter the no.of floors ");
-	scanf("%d",F);
+	printf("enter the no.of floors between 1 to 99\n");
+	scanf("%d",&F);
 	if(F<0 ||F>99)
 	{
 		takeFloors();
 	}
 	else{
-		printf("the no.of.floors are",F);
+		printf("the no.of.floors are %d\n",F);
 	}
         //retake input if required.
   return 0;
@@ -39,25 +42,61 @@ int printDetails()
       //   printf("The current floor number is:- %d The Current state is:- %d The list of commands %d ",f,s,c);
         //e.g.
         //Floor: 0, State: UP, Commands: 203, 105, 7, 100
-         printf("Enter Current floor number ,current state,list of commands\n");
-         scanf("%d %s %d",&currentFloor,&currentState,&Command);
-          printf("Floor:- %d,state:-%s,commands:-%d\n",currentFloor,currentState,Command);
-        return 0;
+    printf("\n The current floor number is: %d \t current state is %d \n", currentFloor,currentState);
+ 
+     Command *p= NULL;
+     p = head;
+    while(p != NULL)
+    {
+		printf("\n The list of (current and pending) commands is: %d\n", p->data);
+		p = p->next;
+    }    
+
+
+    
+return 0;
 }
 
 int takeCommands()
 {
    
-  {
-   
-         Node *p;
-         p = head;
-    while(p != NULL)
-    {
-        printf("\n The current floor number is: %d \t current state is %c \n", currentFloor,currentState);
-		printf("\n The list of (current and pending) commands is: %d", p->data);
-		p = p->next;
+     Command *p = NULL;
+     Command *newNode = NULL;
+     int N = 0, total=0;
+     printf("How many commands  you want: "); scanf("%d", &N);
+     total = N;
+      if(head ==NULL)
+      {
+       head = malloc(sizeof(Command));
+       head->next = NULL;
+       scanf("%d",&(head->data)); // takeData(head);
+      }
+     p = head;
+     while(N > 1)
+     {
+         p->next = malloc(sizeof(Command) );
+         p = p->next;
+         p->next = NULL;
+         scanf("%d",&(p->data));
+         N--;
+     }
+       while(p !=NULL)
+       {
+            {
+        printf("\n The current floor number is: %d \t current state is %d \n", currentFloor,currentState);
+                printf("\n The list of (current and pending) commands is: %d\n", p->data);
+                p = p->next;
     }
+
+
+
+
+
+
+          }
+
+
+
        //print how many commands you want to provide
         //OR
         //4 203 105 7 100 => first number will tell how many commands we should read.
@@ -66,31 +105,11 @@ int takeCommands()
         //Hint: Link list initial filling logic
         return 0;
 
-    }
+    
 }
 
 int processCommandnt()
 {
-         	Command *head = NULL;
-    Command *p = NULL;
-    Command *newNode = NULL;
-    int N = 0, total=0;
-    printf("How many commands  you want: "); scanf("%d", &N);
-    total = N;
-    head = malloc(sizeof(Command) );
-    head->next = NULL;
-    scanf("%d",&(p->data)); // takeData(head);
-    p = head;
-    while(N > 1)
-    {
-        p->next = malloc(sizeof(Command) );
-        p = p->next;
-        p->next = NULL;
-		scanf("%d",&(p->data));
-        // takeData(p);
-        N--;
-    }
-
           //take the first node (take value of head, change head)
         //command = head->data
 
