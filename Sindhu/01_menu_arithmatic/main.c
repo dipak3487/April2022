3127a4d8b499c7bb1d8aec9ed8396a51b29f281a
Shreya Patil <Shreya>
2022-04-21 10:20:03 +0530

    add

--- Sindhu/01_menu_arithmatic/main.c
+++ Sindhu/01_menu_arithmatic/main.c
@@ -3,43 +3,48 @@
 
 
 
-add(int n1, int n2)
+int add(int n1, int n2)
 {
 	return(n1+n2);
 }
 
-subtract(int n1, int n2)
+int subtract(int n1, int n2)
 {
 	return(n1-n2);
 }
 
 
-multiply(int n1, int n2)
+int multiply(int n1, int n2)
 {
 	return(n1*n2);
 }
 
-divide(int n1, int n2)
+int divide(int n1, int n2)
 {
 	return(n1/n2);
 }
 
 
+void printMenu()
+{
+	printf("For addition print 1\n");
+	printf("For subtraction print 2\n");
+	printf("For muliplication print 3\n");
+	printf("For division print 4\n");
+
+}
+
 int main()
 {
     printf("\n\n\t\tSindhu - Menu driven program 01\n\n\n");
-    int choice, n1,n2, i;
-    printf("For addition print 1\n");
-        printf("For subtraction print 2\n");
-        printf("For muliplication print 3\n");
-        printf("For division print 4\n");
-        printf("Enter your choice :  ");
-        scanf("%d",&choice);
-   
+    int choice, num, i;
+    unsigned long int fact;
 
     while(1)
     {
-
+    
+        printf("Enter your choice :  ");
+        scanf("%d",&choice);
         
         switch(choice)
         {
@@ -55,9 +60,28 @@ int main()
                 break;
         
             case 2:
-                printf("Enter the  numbers for subtraction:\n");
-                scanf("%d %d", &n1,&n2);
-             	printf("\n The result is %d",subtract(n1,n2));
+                printf("Enter number:\n");
+                scanf("%d", &num);
+                if(num == 1)
+                printf("\n1 is neither prime nor composite\n\n");
+                for(i = 2; i < num; i++)
+                {
+                    if(num%i == 0)
+                    {
+                        printf("\n%d is not a prime number\n\n", num);
+                        break;
+                    }
+                
+                }
+                /*
+                    Not divisible by any number other 
+                    than 1 and itself
+                */
+                if(i == num) 
+                {
+                    printf("\n\n%d is a Prime number\n\n", num);
+                    break;
+                }
         
             case 3:
                 printf("Enter number:\n");
@@ -68,16 +92,13 @@ int main()
                 else
                     printf("\n\n%d is an Odd number\n\n",num);
                 break;
-
+        
             case 4:
-                printf("Enter numbers to divide:\n");
-                scanf("%d %d", &n1,&n2);
-                printf("\n The result is %d\n",divide(n1,n2));
-                break;
-
-            case 5:
+                printf("\n\n\t\t\tCoding is Fun !\n\n\n");
                 exit(0);    // terminates the complete program execution
         }
     }
-   
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
 }
+
