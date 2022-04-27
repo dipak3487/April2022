62e40171924993fd232151d96fc3564c6b589118
praveen7762 <pks19023@gmail.com>
2022-04-27 11:14:36 +0530

    Committing to Narmada

--- Narmada/Sprint-01/main.c
+++ Narmada/Sprint-01/main.c
@@ -6,7 +6,7 @@ int main()
 {
 	
     printf("\n\n\t\tTelecom OCS(Online Charging System)-- Team Narmada\n\n\n");
-    char num[14];
+    char num[14], num1[14];
     int choice;
     char str[1024];
 	
@@ -41,7 +41,15 @@ int main()
 			}
 
 			while (fgets(str,1024, fp) != NULL) {
-				printf("%s", str);
+				for(int cols = 0; cols<8; cols++){
+					//char s[1024];
+					fscanf(fp, "%s", &str);
+					if(cols == 0){
+						printf("%s\n", str);
+					}
+				}
+				//printf("%s", str);
+			
     			}
 			printf("\n\nChoose your number: ");
 			scanf("%s", &num);
@@ -64,6 +72,8 @@ int main()
 			}
 			if(x == true){
 				printf("Number found in the list\n\n");
+				//printf("Choose second number:");
+				//scanf("%s", &num1);
 			}
 			else{
 				printf("Invalid Number\n\n");
@@ -72,7 +82,7 @@ int main()
 			break;
 
 
-		case 2:
+		case 2:;
 
       			/*option 2: Read number list from Numbers.list
 			2.a. Number1
@@ -82,6 +92,52 @@ int main()
 			2.z. Back
 			On selection of (a..y), print: DIALING.. (sleep for 0-3 seconds), RINGING (0-5 seconds), CONNECTED (for 1-			  60 seconds or till balance is available). HANGUP on any key press and back to the main menu. Try to fit RN			    A (Ring No Answer) if RINGING for > 4 seconds (i.e. for 5 seconds), then go RNA, return.
 			printf("4. \n");*/
+			fopen("sample.txt", "r");
+			
+			printf("You selected %s as your number:\n", num);
+
+			/*while (fgets(str,1024, fp) != NULL) {
+                                for(int cols = 0; cols<4; cols++){
+                                        //char s[1024];
+                                        fscanf(fp, "%s", &str);
+                                        if(cols == 0){
+                                                printf("%s\n", str);
+                                        }
+                                }
+                                //printf("%s", str);
+
+                        }*/
+
+			printf("Choose another number from the list to dial:");
+			scanf("%s", &num1);
+			//printf(num1);
+
+			fseek(fp, 0, SEEK_SET);
+			bool y = false;
+                        while (fgets(str,1024, fp) != NULL) {
+
+
+                                if((strstr(str, num1))){
+                                       //printf("\t\nNumber found in the list\n\n\n");
+                                        y = true;
+                                        break;
+                                }
+                                else{
+                                        //printf("Invalid number\n");
+                                        y = false;
+
+                                }
+                        }
+                        if(y == true){
+                                printf("Number found in the list\n\n");
+                                //printf("Choose second number:");
+                                //scanf("%s", &num1);
+                        }
+                        else{
+                                printf("Invalid Number\n\n");
+                        }
+			fclose(fp);
+			break;
 
 
 		case 3:
