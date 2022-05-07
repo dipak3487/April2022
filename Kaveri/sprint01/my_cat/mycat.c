c17ff650f6e3b80858c137945443648c6201e71e
Madduri Raghuram <MADDURI.RAGHURAM@CAPGEMINI.com>
2022-05-07 16:32:45 +0530

    commiting mycat to Kaveri

--- Kaveri/sprint01/my_cat/mycat.c
+++ Kaveri/sprint01/my_cat/mycat.c
@@ -1,27 +1,61 @@
 #include<fcntl.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<getopt.h>
 
-int main(int argc,char*argv[])
-
+int main(int argc,char *argv[])
 {
-	if(argc!=2){
-		printf("\nThe syntax should as be follow");
-		printf("\nCommandname filestoread\n");
-		exit(1);
+ int opt;
+  
+  static struct option long_options[] = {
+        {"version", no_argument,       0,  'a' },
+        {"help", no_argument,       0,  'h' },
+        { NULL, 0,     NULL, 0 },
+    };
+while((opt = getopt_long(argc, argv, "ah:",long_options,NULL)) != -1)
+{
+    switch (opt) {
+                		
+				case 'a':
+                    			printf("mycat  (Team Kaveri) version 2 \n written by Team Kaveri\n");
+                    			exit(0);
+				break;
+				case 'h':
+					printf("help");
+					exit(0);
+				break;
+				default:
+					printf("Try './mycat -h' for more information.");
+				}
 	}
+	
+	int o = strlen(argv[1]);
+  if (o > 1)
+   {
+	int k=1;
+	//printf("%d",argc);
+	while(k < argc)
+	{
 	int fdold,count;
 	char buffer[2048];
-	fdold = open(argv[1], O_RDONLY);
+	fdold = open(argv[k], O_RDONLY);
 	if(fdold==-1)
 	{
-		printf("cat: %s. No such file or directory\n",argv[1]);
-		exit(1);
-	}
+		printf("cat: %s. No such file or directory\n",argv[k]);
+		//exit(1);
+		}
 	while((count = read(fdold,buffer,sizeof(buffer)))>0)
 	{
 		printf("%s",buffer);
+		//printf("$");
+		memset(buffer,0,sizeof(buffer));
 	}
+	close(fdold);
+	k++;	
+	}
+  }
 	exit(0);
 }
 
