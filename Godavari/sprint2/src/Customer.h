f15f3c0c6843e123d68893b26e7de290fa501300
pramod <ptidke59@gmail.com>
2022-05-26 11:07:14 +0530

    Commiting pramod to Godavari

---
+++ Godavari/sprint2/src/Customer.h
@@ -0,0 +1,18 @@
+#ifndef CUSTOMER_H_INCLUDED
+#define CUSTOMER_H_INCLUDED
+
+// #include <windows.h>
+
+using namespace std;
+
+class Customer{
+    public:
+    string firstname;
+    string lastname;
+    string address;
+    int phone;
+    int num_of_days;
+};
+
+#endif // CUSTOMER_H_INCLUDED
+
---
+++ Godavari/sprint2/src/Room.h
@@ -0,0 +1,26 @@
+#ifndef ROOM_H_INCLUDED
+#define ROOM_H_INCLUDED
+
+// #include <windows.h>
+#include "Customer.h"
+
+using namespace std;
+
+class Room
+{
+    public:
+    string roomtype;
+    string comfort;
+    string capacity;
+    string status;
+    int rent_per_day;
+
+    Customer cust;
+    void addRoom(int);
+    void searchRoom(int);
+    void modifyRoom(int);
+    void displayRoom(int);
+};
+
+#endif // ROOM_H_INCLUDED
+
