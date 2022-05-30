77301a25b0bd7bdd5f76c9717772ae030310866c
Shreya Patil <Shreya>
2022-05-30 13:39:20 +0530

    commit

---
+++ Sindhu/Sprint_02/src/main.cpp
@@ -0,0 +1,75 @@
+/*
+    Filename : main.cpp
+    Created On : 25th May 2022
+    Description : This file takes the input file and calls the functions the user wants to implement. 
+*/
+
+#include<stdio.h>
+#include <iostream>
+#include <string.h>
+#include<sstream>
+#include <cstring>
+#include <sqlite3.h>
+#include<memory>
+#include<bits/stdc++.h>
+#include "vaccine.h"
+using namespace std;
+
+
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+void menu()
+{
+    
+    cout<<"\n\n\t\t\t\xB3\xB2=\xB2=\xB2-\xB3 VACCINE MANAGEMENT SYSTEM  \xB3\xB2=\xB2=\xB2-\xB3\n\n"<<endl;
+    cout<<"\n\t\t\t------------------------------------------------------------------------------------\n";
+    cout<<"\t\t\t\t MAIN MENU";
+    cout<<"\n\t\t\t------------------------------------------------------------------------------------\n";
+    cout<<"\n\t\t01: Add New Record"<<endl;
+    cout<<"\n\t\t02: View VACCINE Inventory"<<endl;
+    cout<<"\n\t\t03: View All Citizen Data"<<endl;
+    cout<<"\n\t\t04: Exit"<<endl;
+}
+int main(int argc, char** argv)
+{
+
+     Vaccine obj;
+     int k=3;
+
+     int ch;
+      do{
+
+            menu();
+            cout<<"\n\t\n\tChoose according to your need : ";
+            cin>>ch;
+
+      switch(ch)
+       {
+         case 1: 
+	             obj.addNew();
+                 break;
+         case 2: 
+       	    obj.view_Vaccine();
+                 break;
+         case 3:
+		   obj.view_all();
+                break;
+          
+         case 4:  
+                    exit(0);
+         default: 
+                  cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\tTHANK YOU!!";
+                  cout<<"\n\n\t\t\t\t\t****HAVE A NICE DAY*****";
+                  exit(0);
+       }
+      }while(ch!=0);
+      return 0;
+}
