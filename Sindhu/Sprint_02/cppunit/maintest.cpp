977e9005c1a18b09b7abd4a40d7298ded7e847d3
Shreya Patil <Shreya>
2022-06-11 10:01:33 +0530

    commit

--- Sindhu/Sprint_02/cppunit/maintest.cpp
+++ Sindhu/Sprint_02/cppunit/maintest.cpp
@@ -1,18 +1,21 @@
 #include<cppunit/ui/text/TestRunner.h>
 #include<cppunit/TestCaller.h>
 #include<iostream>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
 #include<fstream>
 #include "vaccineTest.h"
-#include "../include/vaccine.h"
+#include "vaccine.h"
 
-//using namespace std;
+using namespace std;
+
+
 
 
 int main(int argc,char* argv[])
 {
 	CppUnit::TextUi::TestRunner runner;
-
-    runner.addTest(VaccineTest::suite());
+        runner.addTest(VaccineTest::suite());
 	runner.run();
 	return 0;
 }
--- Sindhu/Sprint_02/cppunit/vaccineTest.h
+++ Sindhu/Sprint_02/cppunit/vaccineTest.h
@@ -1,11 +1,26 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
-#include "../include/vaccine.h"
+#include <cppunit/TestCase.h>
+#include "vaccine.h"
+#include <sqlite3.h>
+
+using namespace std;
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+	int i;
+	for(i = 0; i<argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+
 
 
 class VaccineTest : public CppUnit::TestFixture
 {
-	private:
+
 	public:
 		void setup()
 		{
@@ -16,24 +31,45 @@ class VaccineTest : public CppUnit::TestFixture
 
 		void sunnytest()
 		{	
+			sqlite3* db;
+			char* zErrMsg=0;
+			int rc;
+			int count = 0;
+
+
+		rc = sqlite3_open("example.db", &db);
+		std::ostringstream temp;
+		std::string command;
+
+
+	if( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+
+	} else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+		rc = sqlite3_exec(db, "select count(*) from VACCINE_INVENTORY", callback, &count, &zErrMsg);
+
+
+			if( rc != SQLITE_OK ){
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+			} else {
+			fprintf(stdout, "Records created successfully\n");
+			}
+
 			Vaccine vac;
-			vac.countRecords();
-			CPPUNIT_ASSERT_EQUAL(vac.countRecords(),15);
+			vac.CountRecords();
+			CPPUNIT_ASSERT_EQUAL(vac.CountRecords(),16);
+			sqlite3_close(db);
+
 
 		}
-		/*
-		   void rainytestSearchCitizenRecord()
-		   {
-		   Vaccine vac;
-		   vac.search_name();
-		   std::string str="test/";
-		   CPPUNIT_ASSERT_EQUAL(vac.search_name(str),false);
 
-		   }
-		 */
 		CPPUNIT_TEST_SUITE(VaccineTest);
 		CPPUNIT_TEST(sunnytest);
-//		CPPUNIT_TEST(rainytestSearchCitizenRecord);
+		CPPUNIT_TEST_SUITE_END();
+
 
 };
 
